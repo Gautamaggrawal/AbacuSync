dcf03071c85bd34680c88ad0ee980e5c77a02ab9
Gautam aggrawal <gautamaggrawalsd@yahoo.in>
2025-03-18 01:11:40 +0530

    updated admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Student, StudentLevelHistory, Level
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'get_phone_number', 'get_email', 'gender',
+        'centre', 'ci', 'current_level', 'is_active'
+    )
+    list_filter = (
+        'is_active', 'gender', 'centre', 'current_level',
+        'level_start_date'
+    )
+    search_fields = (
+        'name', 'user__phone_number', 'user__email',
+        'centre__centre_name', 'ci__name'
+    )
+    raw_id_fields = ('user', 'centre', 'ci', 'current_level')
+    date_hierarchy = 'level_start_date'
+
+    def get_phone_number(self, obj):
+        return obj.user.phone_number
+    get_phone_number.short_description = 'Phone Number'
+    get_phone_number.admin_order_field = 'user__phone_number'
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
+    get_email.admin_order_field = 'user__email'
+
+
+@admin.register(StudentLevelHistory)
+class StudentLevelHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'student', 'previous_level', 'new_level',
+        'start_date', 'completion_date', 'changed_by'
+    )
+    list_filter = (
+        'start_date', 'completion_date',
+        'previous_level', 'new_level'
+    )
+    search_fields = (
+        'student__name', 'student__user__phone_number',
+        'previous_level__name', 'new_level__name'
+    )
+    raw_id_fields = (
+        'student', 'previous_level', 'new_level', 'changed_by'
+    )
+    date_hierarchy = 'start_date'
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
--- users/admin.py
+++ users/admin.py
@@ -1,26 +1,21 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from users.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 
 @admin.register(User)
-class UserAdmin(DjangoUserAdmin):
-    
-    list_display = ("phone_number", "email", "user_type", "is_staff", "is_superuser")
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'user_type')
-    search_fields = ("phone_number", "email", "user_type", "uuid")
+class CustomUserAdmin(UserAdmin):
+    list_display = ('phone_number', 'email', 'user_type', 'is_active', 'is_staff')
+    list_filter = ('user_type', 'is_active', 'is_staff')
+    search_fields = ('phone_number', 'email')
     ordering = ('phone_number',)
     
     fieldsets = (
-        (None, {'fields': ('phone_number', 'email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
-        (_('User type'), {'fields': ('user_type',)}),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-        }),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal info', {'fields': ('email', 'user_type')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                  'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     
     add_fieldsets = (
